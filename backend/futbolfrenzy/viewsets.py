426ca852148307f86a113b38f0afd603e0529d6d
kaushalaaditya99 <kaushalaaditya99@gmail.com>
2026-02-21 20:11:41 -0500

    create account class

---
+++ backend/futbolfrenzy/viewsets.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from futbolfrenzy.models import Account
+
+from futbolfrenzy.serializers import AccountSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [AllowAny]
