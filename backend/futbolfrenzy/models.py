fdaea890030ad1aad078f82f432d0040e446ec86
kaushalaaditya99 <kaushalaaditya99@gmail.com>
2026-02-21 20:41:54 -0500

    bug fixes, should compile now

--- backend/futbolfrenzy/models.py
+++ backend/futbolfrenzy/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    id = models.FloatField() #listed as bigint
+    #id = models.FloatField(primary_key = True) #listed as bigint
+    #id is already a basic django class attribute
     typeId = models.FloatField() #listed as bigint
     username = models.CharField(max_length=255)#listed as varchar
     hashpassword = models.CharField(max_length=255)#listed as varchar
