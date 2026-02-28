c129d76f35eb0d6e260d495ac108091a1cc81159
chrisramrothh <gdtrophic@gmail.com>
2026-02-28 14:26:31 -0500

    added viewsets

--- backend/futbolfrenzy/viewsets.py
+++ backend/futbolfrenzy/viewsets.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
-# from futbolfrenzy.models import Account
+from futbolfrenzy.models import Drill, Enrollment
 
-# from futbolfrenzy.serializers import AccountSerializer
+from futbolfrenzy.serializers import DrillSerializer, EnrollmentSerializer
 
 """
 class AccountViewSet(viewsets.ModelViewSet):
@@ -10,3 +10,13 @@ class AccountViewSet(viewsets.ModelViewSet):
     serializer_class = AccountSerializer
     permission_classes = [AllowAny]
 """
+
+class DrillViewSet(viewsets.ModelViewSet):
+    queryset = Drill.objects.all()
+    serializer_class = DrillSerializer
+    permission_classes = [AllowAny]
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+    permission_classes = [AllowAny]
