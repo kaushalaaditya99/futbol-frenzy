92949ce6652ac61526734cca874043e08faa19c0
chrisramrothh <gdtrophic@gmail.com>
2026-02-28 14:22:19 -0500

    added serializers

--- backend/futbolfrenzy/serializers.py
+++ backend/futbolfrenzy/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-#from futbolfrenzy.models import Account
+from futbolfrenzy.models import Drill, Enrollment
 
 #class serializers to help django convert JSON data to python objects
 
@@ -10,3 +10,15 @@ class AccountSerializer(serializers.ModelSerializer):
         fields = ['id', 'typeId', 'username', 'hashpassword']
         read_only_fields = ['id', 'typeId']
         """
+
+class DrillSerializer(serializers.modelSerializer):
+    class Meta:
+        model = Drill
+        fields = ['id', 'drillName', 'coachID']
+        read_only_fields = ['id']
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = ['id', 'studentID', 'coachID', 'drillID', 'assignedDate', 'dueDate', 'completed']
+        read_only_fields = ['id']
