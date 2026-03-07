96d1fb3948c59aaa8d561d06fedec2e9e36958ec
chrisramrothh <gdtrophic@gmail.com>
2026-03-06 22:14:26 -0500

    added URL /api/users/me which gets the json data for self

--- backend/futbolfrenzy/views.py
+++ backend/futbolfrenzy/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Drill
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -8,3 +11,13 @@ def home(request):
         'drills': Drill.objects.all()
     }
     return render (request, 'futbolfrenzy/home.html', context)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def me(request):
+    user = request.user
+    return Response({
+        'id': user.id,
+        'username': user.username,
+        'groups': list(user.groups.values_list('name', flat=True))
+    })
