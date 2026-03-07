c79868cea0ba86fde2bc2bfbfb38ed2467535537
chrisramrothh <gdtrophic@gmail.com>
2026-03-06 21:47:38 -0500

    automatic token creation for new users

--- backend/FrenzyCore/settings.py
+++ backend/FrenzyCore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
 
     #ensure api connection works
     "rest_framework",
--- backend/futbolfrenzy/apps.py
+++ backend/futbolfrenzy/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class FutbolfrenzyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'futbolfrenzy'
+
+    # stuff for token creation with new users
+    def ready(self):
+        import futbolfrenzy.signals
