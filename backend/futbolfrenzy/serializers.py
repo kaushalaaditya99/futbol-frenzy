e04adf2ceb565c7dd24c8c039eb34c2f7d18e315
kaushalaaditya99 <kaushalaaditya99@gmail.com>
2026-03-08 20:50:23 -0400

    fix account validation issue

--- backend/futbolfrenzy/serializers.py
+++ backend/futbolfrenzy/serializers.py
@@ -7,7 +7,19 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
         read_only_fields = ['id']
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        return user
 
 
 class NotificationSerializer(serializers.ModelSerializer):
