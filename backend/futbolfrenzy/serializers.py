a4f832314cadfe59534f5160aa55513dd9ac05ce
Aaditya Kaushal <kaushalaaditya99@gmail.com>
2026-03-30 00:38:54 -0400

    create default settings object with account creation

--- backend/futbolfrenzy/serializers.py
+++ backend/futbolfrenzy/serializers.py
@@ -1,36 +1,57 @@
+from django.contrib.auth.models import Group, User
 from rest_framework import serializers
-from .models import Notification, Settings, Drill, Workout, Assignment, Submission, SubmittedDrill, SoccerClass, ClassMember
-from django.contrib.auth.models import User, Group
-#class serializers to help django convert JSON data to python objects
+
+from .models import (
+    Assignment,
+    ClassMember,
+    Drill,
+    Notification,
+    Settings,
+    SoccerClass,
+    Submission,
+    SubmittedDrill,
+    Workout,
+)
+
+# class serializers to help django convert JSON data to python objects
+
 
 class UserSerializer(serializers.ModelSerializer):
     group = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'group']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-        read_only_fields = ['id']
-    #overwrite User Post function to fix validation issues
+        fields = [
+            "id",
+            "username",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+            "group",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+        read_only_fields = ["id"]
+
+    # overwrite User Post function to fix validation issues
     def create(self, validated_data):
 
-        
         user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            username=validated_data["username"],
+            password=validated_data["password"],
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
         )
 
-        #log user with relevant group(does it need logic to validate presence of Student and Coach group?)
-        
-        #adds user to group, will create group if it doesnt exist
-        my_group, created = Group.objects.get_or_create(name=validated_data['group'])
+        # log user with relevant group(does it need logic to validate presence of Student and Coach group?)
+
+        # adds user to group, will create group if it doesnt exist
+        my_group, created = Group.objects.get_or_create(name=validated_data["group"])
         user.groups.add(my_group)
-        
+
+        user_default_settings = Settings.objects.create(userID=user, mode="light")
+
         return user
 
 
@@ -43,57 +64,103 @@ class NotificationSerializer(serializers.ModelSerializer):
 class SettingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Settings
-        fields = ['id', 'userID', 'mode', 'notificationType', 
-                  'profilePicture', 'profileBackgroundColor', 'position']
-        read_only_fields = ['id']
+        fields = [
+            "id",
+            "userID",
+            "mode",
+            "notificationType",
+            "profilePicture",
+            "profileBackgroundColor",
+            "position",
+        ]
+        read_only_fields = ["id"]
 
 
 class DrillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drill
-        fields = ['id', 'drillName', 'drillType', 'coachID', 'url',
-                  'time', 'difficultyLevel', 'instructions', 'imageBackgroundColor',
-                  'imageText', 'imageTextColor', 'publicDrill']
-        read_only_fields = ['id']
+        fields = [
+            "id",
+            "drillName",
+            "drillType",
+            "coachID",
+            "url",
+            "time",
+            "difficultyLevel",
+            "instructions",
+            "imageBackgroundColor",
+            "imageText",
+            "imageTextColor",
+            "publicDrill",
+        ]
+        read_only_fields = ["id"]
 
 
 class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workout
-        fields = ['id', 'workoutName', 'workoutType', 'coachID', 'dueDate',
-                  'imageBackgroundColor','imageText', 'imageTextColor', 'drills', 'publicWorkout']
-        read_only_fields = ['id']
+        fields = [
+            "id",
+            "workoutName",
+            "workoutType",
+            "coachID",
+            "dueDate",
+            "imageBackgroundColor",
+            "imageText",
+            "imageTextColor",
+            "drills",
+            "publicWorkout",
+        ]
+        read_only_fields = ["id"]
+
 
 class AssignmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Assignment
-        fields = ['id', 'workoutID', 'dueDate', 
-                  'imageBackgroundColor','imageText', 'imageTextColor']
-        read_only_fields = ['id']
+        fields = [
+            "id",
+            "workoutID",
+            "dueDate",
+            "imageBackgroundColor",
+            "imageText",
+            "imageTextColor",
+        ]
+        read_only_fields = ["id"]
+
 
 class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
-        fields = ['id', 'studentID', 'assignmentID', 'grade',
-                  'dateGraded', 'dateSubmitted', 
-                  'imageBackgroundColor','imageText', 'imageTextColor']
-        read_only_fields = ['id']
+        fields = [
+            "id",
+            "studentID",
+            "assignmentID",
+            "grade",
+            "dateGraded",
+            "dateSubmitted",
+            "imageBackgroundColor",
+            "imageText",
+            "imageTextColor",
+        ]
+        read_only_fields = ["id"]
+
 
 class SubmittedDrillSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubmittedDrill
-        fields = ['id', 'submissionID', 'drillID', 
-                  'videoURL','grade', 'touchCount']
-        read_only_fields = ['id']
+        fields = ["id", "submissionID", "drillID", "videoURL", "grade", "touchCount"]
+        read_only_fields = ["id"]
+
 
 class SoccerClassSerializer(serializers.ModelSerializer):
     class Meta:
         model = SoccerClass
-        fields = ['id', 'className', 'coachID', 'assignments']
-        read_only_fields = ['id']
+        fields = ["id", "className", "coachID", "assignments"]
+        read_only_fields = ["id"]
+
 
 class ClassMemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassMember
-        fields = ['id', 'studentID', 'classID']
-        read_only_fields = ['id']
+        fields = ["id", "studentID", "classID"]
+        read_only_fields = ["id"]
