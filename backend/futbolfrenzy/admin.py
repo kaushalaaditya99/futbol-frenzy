62acba4cb90068a6bd0f5ae28f1b3a099ecf2f28
chrisramrothh <gdtrophic@gmail.com>
2026-03-31 14:03:39 -0400

    add bookmarks to admin view, also the migration i forgot to commit for the model

--- backend/futbolfrenzy/admin.py
+++ backend/futbolfrenzy/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Notification, Settings, Drill, Workout, Assignment, Submission, SubmittedDrill, SoccerClass, ClassMember
+from .models import Notification, Settings, Drill, DrillBookmark, Workout, WorkoutBookmark, Assignment, Submission, SubmittedDrill, SoccerClass, ClassMember
 
 # Register your models here.
 admin.site.register(Notification)
 admin.site.register(Settings)
 admin.site.register(Drill)
+admin.site.register(DrillBookmark)
 admin.site.register(Workout)
+admin.site.register(WorkoutBookmark)
 admin.site.register(Assignment)
 admin.site.register(Submission)
 admin.site.register(SubmittedDrill)
