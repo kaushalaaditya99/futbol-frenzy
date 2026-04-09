1784bda4f312fa0ff7cea7199a8d37baae097b76
DarrenChen2025 <DarrenChenCS@gmail.com>
2026-04-09 13:27:54 -0400

    Fixed the create class button to rerender the page to show the newly created class

--- frontend/app/createClass.tsx
+++ frontend/app/createClass.tsx
@@ -97,7 +97,7 @@ export default function CreateClass() {
         );
 
         if (successful) {
-            router.back();
+            router.replace("/(tabs)/classes");
             return;
         }
 
